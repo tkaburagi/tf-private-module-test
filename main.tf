e67c1359f1d93a5b3687c2b5fa7b852b79dbb5e2
Takayuki Kaburagi <t.kaburagi@me.com>
2019-08-11 16:31:22 +0900

    added module

--- main.tf
+++ main.tf
@@ -1,11 +1,23 @@
-	terraform {
-	  required_version = " 0.12.6"
-	  backend "remote" {
-	    hostname = "app.terraform.io"
-	    organization = "tkaburagi"
+terraform {
+  required_version = " 0.12.6"
+  backend "remote" {
+	hostname = "app.terraform.io"
+	organization = "tkaburagi"
 
-	    workspaces {
-	      name = "private-module-test"
-	    }
-	  }
+	workspaces {
+	  name = "private-module-test"
 	}
+  }
+}
+
+provider "aws" {
+	access_key = var.access_key
+	secret_key = var.secret_key
+	region = var.region
+}
+
+module "vpc" {
+	source = "app.terraform.io/tkaburagi/vpc/aws"
+	version = "0.0.1"
+	vpc_cidr = "10.0.0.1/24"
+}
